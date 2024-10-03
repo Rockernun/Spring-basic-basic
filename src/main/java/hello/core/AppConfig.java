3090041fb56e1f5fb0c8705d943a7a7bc16b068e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-10-03 21:35:50 +0900

    refactor : AppConfig 리팩토링
    원래 코드를 보면 MemoryMemberRepository가 중복되고, 역할에 따른 구현이 잘 안보였었다.
    
    new MemoryMemberRepository 부분의 중복을 제거하고, 이제 MemoryMemberRepository를 다른 구현체로 변경할 때 한 부분만 변경하면 된다.
    
    변경된 AppConfig는 역할과 구현 클래스가 한눈에 들어온다. 애플리케이션 전체 구성이 어떻게 되어 있는지 빠르게 파악할 수 있게 되었다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,11 +10,23 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+    // MemberService 역할
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    // MemberRepository 역할
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // OrderService 역할
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    
+    //  DiscountPolicy 역할
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
